b51911e7c22961b0f9368dc345dc67f6a33a5a13
errcricket <err29@drexel.edu>
2014-12-11 07:56:22 -0500

    Adding output data

--- copy_images.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-cp /data/erin/Project_GC/Analysis/images/* images/.
-cp /data/erin/Project_GC/Analysis/correlations.txt .
-cp /data/erin/Project_GC/Analysis/HP_distribution.txt .
-cp /data/erin/Project_GC/Analysis/formatted_significance.txt .
-cp /data/erin/Project_GC/Report_Scripts/mergedStatReport.txt .
